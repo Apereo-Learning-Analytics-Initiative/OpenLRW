61576bc0f4d1077a5e905a5d7476e4d240270f62
nisithdash <nisithdash@gmail.com>
2017-05-23 12:09:34 -0700

    LAQ-207 - Create LRW Admin Users
    Create LRW Admin Users

--- src/main/java/unicon/matthews/admin/AdminUserAuthenticationProvider.java
+++ src/main/java/unicon/matthews/admin/AdminUserAuthenticationProvider.java
@@ -3,6 +3,8 @@ package unicon.matthews.admin;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
@@ -12,43 +14,56 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import unicon.matthews.admin.service.AdminUserService;
 import unicon.matthews.oneroster.service.OrgService;
 import unicon.matthews.security.model.UserContext;
 
 @Component
 public class AdminUserAuthenticationProvider implements AuthenticationProvider {
-  
-  private final AdminUserService adminUserService;
-  private final OrgService orgService;
 
-  @Autowired
-  public AdminUserAuthenticationProvider(AdminUserService adminUserService, OrgService orgService) {
-    super();
-    this.adminUserService = adminUserService;
-    this.orgService = orgService;
-  }
+    private static Logger logger = LoggerFactory.getLogger(AdminUserAuthenticationProvider.class);
 
-  @Override
-  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    Assert.notNull(authentication, "No authentication data provided");
+    @Autowired
+    private AdminUserService adminUserService;
 
-    String username = (String) authentication.getPrincipal();
-    String password = (String) authentication.getCredentials();
-    
-    // Do the check of user/pass with admin user service 
-    
-    // If found
-    // -- look up org / tenant
-    // -- determine if super or org admin and set authority (ROLE_ORG_ADMIN, ROLE_SUPER_ADMIN)
-    // hardcoded for testing purposes
-    List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_ORG_ADMIN"));        
-    UserContext userContext = UserContext.create("TENANTID", "ORGID", authorities);        
-    return new AdminUserAuthenticationToken(userContext, null, userContext.getAuthorities());
-  }
+    @Autowired
+    private OrgService orgService;
 
-  @Override
-  public boolean supports(Class<?> authentication) {
-    return (AdminUserAuthenticationToken.class.isAssignableFrom(authentication));
-  }
+    /**
+     * Authenticate admin user
+     *
+     * @param authentication
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        Assert.notNull(authentication, "No authentication data provided");
+        UserContext userContext = null;
+        String username = (String) authentication.getPrincipal();
+        String password = (String) authentication.getCredentials();
+        try {
+            AdminUser adminUser = adminUserService.authenticateUser(username, password);
+            if (adminUser != null) {
+                String tenantId = adminUser.getTenantId();
+                String orgId = adminUser.getOrgId();
+                if (adminUser.isSuperAdmin()) {
+                    List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_SUPER_ADMIN"));
+                    userContext = UserContext.create(tenantId, orgId, authorities);
+                } else {
+                    List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_ORG_ADMIN"));
+                    userContext = UserContext.create(tenantId, orgId, authorities);
+                }
+            }
+        } catch (AuthenticationException ex) {
+            logger.error(String.format("Unable to authenticate user=%s. Invalid credentials supplied ", username) + ex.getMessage(), ex);
+            throw ex;
+        }
+        return new AdminUserAuthenticationToken(userContext, null, userContext.getAuthorities());
+    }
 
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (AdminUserAuthenticationToken.class.isAssignableFrom(authentication));
+    }
 }
