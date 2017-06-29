a0c1013da097cb0ced507a4e6c2c8c7c14eba1dc
Gary Gilbert <gjgilbert@gmail.com>
2017-06-29 12:10:40 -0400

    first part of performance enhancements

--- src/main/java/unicon/matthews/oneroster/service/UserService.java
+++ src/main/java/unicon/matthews/oneroster/service/UserService.java
@@ -1,6 +1,5 @@
 package unicon.matthews.oneroster.service;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -9,13 +8,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import unicon.matthews.Vocabulary;
-import unicon.matthews.oneroster.Enrollment;
 import unicon.matthews.oneroster.User;
-import unicon.matthews.oneroster.exception.EnrollmentNotFoundException;
 import unicon.matthews.oneroster.exception.UserNotFoundException;
 import unicon.matthews.oneroster.service.repository.MongoUser;
 import unicon.matthews.oneroster.service.repository.MongoUserRepository;
@@ -25,13 +21,10 @@ public class UserService {
   private static Logger logger = LoggerFactory.getLogger(UserService.class);
   
   private MongoUserRepository mongoUserRepository;
-  private EnrollmentService enrollmentService;
   
   @Autowired
-  public UserService(MongoUserRepository mongoUserRepository,
-      EnrollmentService enrollmentService) {
+  public UserService(MongoUserRepository mongoUserRepository) {
     this.mongoUserRepository = mongoUserRepository;
-    this.enrollmentService = enrollmentService;
   }
 
   public User findBySourcedId(final String tenantId, final String orgId, final String userSourcedId) throws UserNotFoundException {
@@ -75,8 +68,6 @@ public class UserService {
     
     MongoUser saved = mongoUserRepository.save(mongoUserToSave);
     
-//    updateEnrollments(tenantId, orgId, saved);
-    
     return saved.getUser();
   }
 
@@ -117,33 +108,4 @@ public class UserService {
     return user;
 
   }
-
-  @Async
-  public void updateEnrollments(String tenantId, String orgId, MongoUser mongoUser) {
-    try {
-      Collection<Enrollment> userEnrollments = enrollmentService.findEnrollmentsForUser(tenantId, orgId, mongoUser.getUser().getSourcedId());
-      
-      if (userEnrollments != null) {
-        for (Enrollment enrollment : userEnrollments) {
-          Enrollment updatedEnrollment
-            = new Enrollment.Builder()
-              .withKlass(enrollment.getKlass())
-              .withMetadata(enrollment.getMetadata())
-              .withPrimary(enrollment.isPrimary())
-              .withRole(enrollment.getRole())
-              .withSourcedId(enrollment.getSourcedId())
-              .withStatus(enrollment.getStatus())
-              .withUser(mongoUser.getUser())
-              .build();
-          
-          enrollmentService.save(tenantId, orgId, updatedEnrollment);
-        }
-      }
-      
-    } 
-    catch (EnrollmentNotFoundException e) {
-      logger.info("No enrollments found for user");
-    }
-  }
-
 }
