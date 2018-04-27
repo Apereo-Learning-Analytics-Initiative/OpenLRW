f7dc829da72a53672ffff587744ec4feff9fe7a4
xchopin <xchopin54@gmail.com>
2018-04-27 16:21:21 +0200

    Fixed inheritance

--- src/main/java/unicon/matthews/oneroster/exception/LineItemNotFoundException.java
+++ src/main/java/unicon/matthews/oneroster/exception/LineItemNotFoundException.java
@@ -1,14 +1,14 @@
-/**
- * 
- */
 package unicon.matthews.oneroster.exception;
 
 /**
  * @author ggilbert
- *
+ * @author xchopin <xavier.chopin@univ-lorraine.fr>
  */
-public class LineItemNotFoundException extends Exception {
+public class LineItemNotFoundException extends OneRosterNotFoundException {
 
   private static final long serialVersionUID = 1L;
 
+  public LineItemNotFoundException(String message) {
+    super(message);
+  }
 }
--- src/main/java/unicon/matthews/oneroster/exception/OrgNotFoundException.java
+++ src/main/java/unicon/matthews/oneroster/exception/OrgNotFoundException.java
@@ -1,14 +1,14 @@
-/**
- * 
- */
 package unicon.matthews.oneroster.exception;
 
 /**
  * @author ggilbert
- *
+ * @author xchopin <xavier.chopin@univ-lorraine.fr>
  */
-public class OrgNotFoundException extends Exception {
+public class OrgNotFoundException extends OneRosterNotFoundException {
 
   private static final long serialVersionUID = 1L;
 
+  public OrgNotFoundException(String message) {
+    super(message);
+  }
 }
