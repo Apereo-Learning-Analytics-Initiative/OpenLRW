0e643dd225f4e74b336d15f18ef17c34f1af1d09
xchopin <xchopin54@gmail.com>
2018-05-07 11:53:18 +0200

    Added MongoTemplate

--- src/test/java/unicon/matthews/FongoConfig.java
+++ src/test/java/unicon/matthews/FongoConfig.java
@@ -1,19 +1,17 @@
-/**
- * 
- */
 package unicon.matthews;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 
 /**
  * @author ggilbert
- *
+ * @author xchopin <xavier.chopin@univ-lorraine.fr>
  */
 @Configuration
 @EnableMongoRepositories
@@ -34,5 +32,10 @@ public class FongoConfig extends AbstractMongoConfiguration {
   @Override
   protected String getMappingBasePackage() {
       return "unicon.matthews";
+  }
+
+  @Bean
+  public MongoTemplate mongoTemplate() throws Exception {
+      return new MongoTemplate(mongo(), getDatabaseName());
   }
 }
