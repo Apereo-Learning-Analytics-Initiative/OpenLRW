2b3140de33710dc627570d68a2edb24b595a8ad5
xchopin <xchopin54@gmail.com>
2018-11-30 14:03:01 +0100

    feat: added POST method

--- src/main/java/unicon/matthews/oneroster/endpoint/LineItemController.java
+++ src/main/java/unicon/matthews/oneroster/endpoint/LineItemController.java
@@ -2,12 +2,14 @@ package unicon.matthews.oneroster.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import unicon.matthews.entity.MongoClassMappingRepository;
 import unicon.matthews.oneroster.Class;
+import unicon.matthews.oneroster.LineItem;
 import unicon.matthews.oneroster.exception.LineItemNotFoundException;
 import unicon.matthews.oneroster.service.LineItemService;
 import unicon.matthews.oneroster.service.repository.MongoLineItem;
@@ -37,7 +39,7 @@ public class LineItemController {
     /**
      * GET /api/lineitems
      *
-     * Returns all the line items for a tenant id and an organization id given.
+     * Return all the line items for a tenant id and an organization id given.
      * @param token a JWT to get authenticated
      * @return Collection<MongoLineItem> the line items
      * @throws IllegalArgumentException
@@ -48,5 +50,23 @@ public class LineItemController {
         return lineItemService.findAll(userContext.getTenantId(), userContext.getOrgId());
     }
 
+
+    /**
+     * POST /api/lineitems
+     *
+     * Create a line item
+     * @param token a JWT to get authenticated
+     * @param lineItem the object to build
+     * @return LineItem the object created
+     * @throws IllegalArgumentException
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> postLineItem(JwtAuthenticationToken token, @RequestBody LineItem lineItem) throws IllegalArgumentException {
+        UserContext userContext = (UserContext) token.getPrincipal();
+        LineItem savedLineItem = this.lineItemService.save(userContext.getTenantId(), userContext.getOrgId(), lineItem, true);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedLineItem.getSourcedId()).toUri());
+        return new ResponseEntity<>(savedLineItem, httpHeaders, HttpStatus.CREATED);
+    }
 
 }
