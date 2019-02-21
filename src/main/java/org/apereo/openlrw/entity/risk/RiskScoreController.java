b675364a4700990e5cb6a20de8fd6c876009e166
xchopin <xchopin54@gmail.com>
2019-02-21 11:35:56 +0100

    refractor: change route name

--- src/main/java/org/apereo/openlrw/entity/risk/RiskScoreController.java
+++ src/main/java/org/apereo/openlrw/entity/risk/RiskScoreController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/risk")
+@RequestMapping("/api/risks")
 public class RiskScoreController {
 
   private MongoRiskScoreRepository mongoRiskScoreRepository;
