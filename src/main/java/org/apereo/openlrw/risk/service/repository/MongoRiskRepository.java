b105521a32fbd518ff615370ef67094837efdd58
xchopin <xchopin54@gmail.com>
2019-02-25 16:43:09 +0100

    feat: add find method

--- src/main/java/org/apereo/openlrw/risk/service/repository/MongoRiskRepository.java
+++ src/main/java/org/apereo/openlrw/risk/service/repository/MongoRiskRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
+/**
+ * @author xchopin <xavier.chopin@univ-lorraine.fr>
+ */
 public interface MongoRiskRepository extends MongoRepository<MongoRisk, String> {
   List<MongoRisk> findByTenantIdAndOrgIdAndClassSourcedIdAndActive(String tenantId, String orgId, String classSourcedId, boolean active);
   MongoRisk findTopByTenantIdAndOrgIdAndUserSourcedIdAndClassSourcedIdAndActiveOrderByDateTime(String tenantId, String orgId, String userSourcedId, String classSourcedId, boolean active);
+  MongoRisk findByTenantIdAndOrgIdAndUserSourcedIdAndClassSourcedIdAndName(String tenantId, String orgId, String userSourcedId, String classSourcedId, String name);
+
 }
