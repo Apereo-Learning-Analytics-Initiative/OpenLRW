b0329d633f345209f9df7a4400a37b62677c1e81
Xavier Chopin <xchopin54@gmail.com>
2019-03-20 11:26:40 +0100

    refractor: enable tests for production mode

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -5,5 +5,5 @@ DESTINATION_FILE=../../lib/openlrw.jar
 cd `dirname $0`
 cd src/OpenLRW
 git pull
-mvn -DskipTests=true clean install
+mvn -DskipTests=false clean install
 cp $JAR_FILE $DESTINATION_FILE
