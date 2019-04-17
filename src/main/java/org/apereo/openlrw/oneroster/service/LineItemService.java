ffe5c60eb685150d2a2c736cfb484b1e2af0f422
Steven Cody <scody@unicon.net>
2019-04-17 10:46:17 -0600

    debug

--- src/main/java/org/apereo/openlrw/oneroster/service/LineItemService.java
+++ src/main/java/org/apereo/openlrw/oneroster/service/LineItemService.java
@@ -8,7 +8,7 @@ import org.apereo.openlrw.oneroster.service.repository.MongoLineItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sun.tools.sjavac.Log;
+//import com.sun.tools.sjavac.Log;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
