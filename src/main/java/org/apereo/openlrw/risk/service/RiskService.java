5fcb76cc08f20cdf86120e03e629ab1a8673684b
xchopin <xchopin54@gmail.com>
2019-08-22 11:37:47 +0200

    feat(risk): add findByClass() method

--- src/main/java/org/apereo/openlrw/risk/service/RiskService.java
+++ src/main/java/org/apereo/openlrw/risk/service/RiskService.java
@@ -118,41 +118,59 @@ public class RiskService {
      * @param tenantId
      * @param orgId
      * @param classId
+     * @param date : format 'yyyy-mm-dd' or keyword 'latest'
+     * @return Collection<MongoRisk>
+     */
+    public Collection<MongoRisk> getRisksForClass(final String tenantId, final String orgId, final String classId, final String date, final int limit){
+        if (StringUtils.isBlank(tenantId) || StringUtils.isBlank(orgId) || StringUtils.isBlank(classId))
+            throw new IllegalArgumentException();
+
+        Collection<MongoRisk> mongoRisks;
+
+        Query query = new Query();
+        query.with(new Sort(Sort.Direction.DESC, "dateTime")); // Order by date: the most recent
+        query.addCriteria(where("classSourcedId").is(classId).and("orgId").is(orgId).and("tenantId").is(tenantId));
+
+        if (limit > 0)
+            query.limit(limit);
+
+        this.dayCriteria(query, date);
+        mongoRisks = mongoOps.find(query, MongoRisk.class);
+
+        if (!mongoRisks.isEmpty())
+            return new ArrayList<>(mongoRisks);
+
+        throw new OneRosterNotFoundException("Risks not found.");
+    }
+
+
+    /**
+     * Get RiskScore for a user and a class given
+     *
+     * @param tenantId
+     * @param orgId
+     * @param classId
      * @param userId
-     * @param date
+     * @param date : format 'yyyy-mm-dd' or keyword 'latest'
      * @return Collection<MongoRisk>
      */
     public Collection<MongoRisk> getRisksForUserAndClass(
             final String tenantId, final String orgId, final String classId,
             final String userId, final String date, final int limit
-            ) {
+    ){
         if (StringUtils.isBlank(tenantId) || StringUtils.isBlank(orgId) || StringUtils.isBlank(userId) || StringUtils.isBlank(classId))
             throw new IllegalArgumentException();
-
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
         Collection<MongoRisk> mongoRisks;
 
         Query query = new Query();
-        query.with(new Sort(Sort.Direction.DESC, "dateTime")); // Order by date: the more recent
+        query.with(new Sort(Sort.Direction.DESC, "dateTime")); // Order by date: the most recent
         query.addCriteria(where("userSourcedId").is(userId).and("classSourcedId").is(classId).and("orgId").is(orgId).and("tenantId").is(tenantId));
 
         if (limit > 0)
             query.limit(limit);
 
-        if (!date.isEmpty()) {
-                if (date.equals("latest")){
-                    query.limit(1);
-                } else {
-                    try {
-                        DateTime startDate = formatter.parseDateTime(date).withZone(DateTimeZone.UTC);
-                        DateTime endDate = startDate.plusDays(1);
-                        query.addCriteria(where("dateTime").gte(startDate).lt(endDate)); // Get the risks for the day given
-                    } catch (Exception e) {
-                        throw new BadRequestException("Not able to parse the date, it has to be in the following format: `yyyy-MM-dd hh:mm` ");
-                    }
-                }
-        }
+        this.dayCriteria(query, date);
 
         mongoRisks = mongoOps.find(query, MongoRisk.class);
 
@@ -160,6 +178,30 @@ public class RiskService {
             return new ArrayList<>(mongoRisks);
 
         throw new OneRosterNotFoundException("Risks not found.");
+    }
+
+
+    /**
+     * Add a criteria to get the Risks for the given day
+     *
+     * @param query
+     * @param date yyyy-MM-dd or latest
+     */
+    private void dayCriteria(Query query, String date){
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+        if (!date.isEmpty()) {
+            if (date.equals("latest")){
+                query.limit(1);
+            } else {
+                try {
+                    DateTime startDate = formatter.parseDateTime(date).withZone(DateTimeZone.UTC);
+                    DateTime endDate = startDate.plusDays(1);
+                    query.addCriteria(where("dateTime").gte(startDate).lt(endDate)); // Get the risks for the day given
+                } catch (Exception e) {
+                    throw new BadRequestException("Not able to parse the date, it has to be in the following format: `yyyy-MM-dd` ");
+                }
+            }
+        }
     }
 }
 
