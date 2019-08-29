1e53547d46f471efd33c8f514a6e229d837afef7
xchopin <xchopin54@gmail.com>
2019-08-29 15:48:42 +0200

    feat(events): add route to find by source

--- src/main/java/org/apereo/openlrw/events/endpoint/EventController.java
+++ src/main/java/org/apereo/openlrw/events/endpoint/EventController.java
@@ -1,5 +1,6 @@
 package org.apereo.openlrw.events.endpoint;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.openlrw.caliper.Event;
 import org.apereo.openlrw.events.service.EventService;
 import org.apereo.openlrw.oneroster.service.OrgService;
@@ -8,10 +9,7 @@ import org.apereo.openlrw.security.model.UserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -45,7 +43,26 @@ public class EventController {
            @RequestParam(value = "limit", required = false, defaultValue = "1000") String limit
    ) throws Exception {
      UserContext userContext = (UserContext) token.getPrincipal();
-     return eventService.findAll(userContext.getTenantId(), userContext.getOrgId(), page, limit);
+         return eventService.findAll(userContext.getTenantId(), userContext.getOrgId(), page, limit);
    }
+
+    /**
+     * Get the events by EdApp
+     *
+     * @param token
+     * @param page
+     * @param limit
+     * @return
+     */
+    @RequestMapping(value = "/sources/{edAppId:.+}", method = RequestMethod.GET)
+    public Collection<Event> getEvents(
+            JwtAuthenticationToken token,
+            @PathVariable final String edAppId,
+            @RequestParam(value = "page", required = false, defaultValue = "0") String page,
+            @RequestParam(value = "limit", required = false, defaultValue = "1000") String limit
+    ) throws Exception {
+        UserContext userContext = (UserContext) token.getPrincipal();
+        return eventService.findByEdApp(userContext.getTenantId(), userContext.getOrgId(), page, limit, edAppId);
+    }
 
 }
