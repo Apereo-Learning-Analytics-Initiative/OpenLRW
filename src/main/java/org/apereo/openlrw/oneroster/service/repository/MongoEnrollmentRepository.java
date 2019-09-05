9d2363f11752d06c70a8cdc3ad7403fb27525825
xchopin <xchopin54@gmail.com>
2019-09-05 11:18:06 +0200

    feat: add methods to find enrollments by criterias

--- src/main/java/org/apereo/openlrw/oneroster/service/repository/MongoEnrollmentRepository.java
+++ src/main/java/org/apereo/openlrw/oneroster/service/repository/MongoEnrollmentRepository.java
@@ -2,26 +2,25 @@ package org.apereo.openlrw.oneroster.service.repository;
 
 import org.apereo.model.oneroster.Role;
 import org.apereo.model.oneroster.Status;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author ggilbert
  * @author xchopin <xavier.chopin@univ-lorraine.fr>
  */
 public interface MongoEnrollmentRepository extends MongoRepository<MongoEnrollment, String> {
-  Collection<MongoEnrollment> findByTenantIdAndOrgIdAndUserSourcedIdIgnoreCaseAndEnrollmentStatus(final String tenantId, final String orgId, final String userSourcedId, final Status status);
-  
-  Collection<MongoEnrollment> findByTenantIdAndOrgIdAndClassSourcedIdAndEnrollmentStatus(final String tenantId, final String orgId, final String classSourcedId, final Status status);
-  
   MongoEnrollment findByTenantIdAndOrgIdAndClassSourcedIdAndUserSourcedIdIgnoreCase(final String tenantId, final String orgId, final String classSourcedId, final String userSourcedId);
-  
   Integer countByTenantIdAndOrgIdAndClassSourcedIdAndEnrollmentStatusAndEnrollmentRole(final String tenantId, final String orgId, final String classSourcedId, final Status status, final Role role);
-  
+  Collection<MongoEnrollment> findByTenantIdAndOrgIdAndUserSourcedIdIgnoreCaseAndEnrollmentStatus(final String tenantId, final String orgId, final String userSourcedId, final Status status);
+  Collection<MongoEnrollment> findByTenantIdAndOrgIdAndClassSourcedIdAndEnrollmentStatus(final String tenantId, final String orgId, final String classSourcedId, final Status status);
   Collection<MongoEnrollment> findByTenantIdAndOrgIdAndEnrollmentRole(final String tenantId, final String orgId, final String Role);
-
+  List<MongoEnrollment> findTopByTenantIdAndOrgIdOrderByEnrollmentBeginDateDesc(final String tenantId, final String orgId, Pageable pageRequest);
+  List<MongoEnrollment> findTopByTenantIdAndOrgIdOrderByEnrollmentEndDateDesc(final String tenantId, final String orgId, Pageable pageRequest);
+  List<MongoEnrollment> findTopByTenantIdAndOrgId(final String tenantId, final String orgId, Pageable pageRequest);
   Long deleteByTenantIdAndOrgIdAndEnrollmentSourcedId(String tenantId, String orgId, String sourcedId);
-
   Long deleteAllByTenantIdAndOrgId(String tenantId, String orgId);
 }
