63f7fb015aca08ffeac2a017e94c1793d7375148
xchopin <xchopin54@gmail.com>
2019-09-09 16:30:58 +0200

    feat: add pageable method

--- src/main/java/org/apereo/openlrw/oneroster/service/repository/MongoResultRepository.java
+++ src/main/java/org/apereo/openlrw/oneroster/service/repository/MongoResultRepository.java
@@ -1,19 +1,20 @@
 package org.apereo.openlrw.oneroster.service.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author ggilbert
+ * @author xchopin <xavier.chopin@univ-lorraine.fr>
+ */
 
 public interface MongoResultRepository extends MongoRepository<MongoResult, String> {
-  Collection<MongoResult> 
-  findByTenantIdAndOrgIdAndClassSourcedId(final String tenantId, final String orgId,
-    final String classSourcedId);
-  
+  Collection<MongoResult> findByTenantIdAndOrgIdAndClassSourcedId(final String tenantId, final String orgId, final String classSourcedId);
   MongoResult findByTenantIdAndOrgIdAndResultSourcedId(final String tenantId, final String orgId, final String resultSourcedId);
-  
   Collection<MongoResult> findByTenantIdAndOrgIdAndLineitemSourcedId(final String tenantId, final String orgId, final String lineItemSourcedId);
-
-  MongoResult
-  findByTenantIdAndOrgIdAndUserSourcedId(final String tenantId, final String orgId, final String userSourcedId);
-
+  MongoResult findByTenantIdAndOrgIdAndUserSourcedId(final String tenantId, final String orgId, final String userSourcedId);
+  List<MongoResult> findTopByTenantIdAndOrgIdOrderByResultDateDesc(final String tenantId, final String orgId, Pageable pageRequest);
 }
